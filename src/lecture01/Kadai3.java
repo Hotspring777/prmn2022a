fd93fa21e30bf6a108eff42b4598cd40205ef2b8
Hotspring777 <cist.b220.t.kamada@gmail.com>
2022-11-01 14:14:42 +0900

    課題3

---
+++ src/lecture01/Kadai3.java
@@ -0,0 +1,13 @@
+package lecture01;
+
+public class Kadai3 {
+    public static void main(String[] args) {
+        int age=20;
+
+        if(age<20){  //ageが20未満の時
+            System.out.println("I am "+age+" years old so I cannot drink liquor.");
+        }else{  //ageが20以上の時
+            System.out.println("I am "+age+" years old so I can drink liquor.");
+        }
+    }
+}
