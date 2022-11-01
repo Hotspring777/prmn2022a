9bff0283d643fa7d67e7a0cc7a062a18f176e440
Hotspring777 <cist.b220.t.kamada@gmail.com>
2022-11-01 14:09:30 +0900

    課題2

---
+++ src/lecture01/Kadai2.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class Kadai2 {
+    public static void main(String[] args) {
+        int studentNumber = 2200800;
+        System.out.println("B"+studentNumber+" Toshinobu Kamada");
+    }
+}
